29e7881064fa7f98141c3d6639815b5353116a29
Gonzalo <gonzalo.betancourt.92@hotmail.com>
2015-12-05 12:39:44 -0500

    Creating Orders - Part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+	devise_for :users
 	resources :products
 	resource :cart, only: [ :show ] do
 		post "add", path: "add/:id"
+		get :checkout
 	end
+
+	resources :orders, only: [ :index, :show, :create ]
 	root 'products#index'
 end
